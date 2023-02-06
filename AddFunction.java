ecd8e8dd31c446a44b8f62c41c923e8af8358d54
maha <maha@gmail.com>
2023-02-06 08:38:35 +0530

    my task completed

--- AddFunction.java
+++ AddFunction.java
@@ -1,3 +1,7 @@
 this is my add function 
 my manager work completed 
-
+1st day java lines of code 
+2nd day java line of code 
+3rd day java line of code
+4th day java line of code 
+5th day java line of code
